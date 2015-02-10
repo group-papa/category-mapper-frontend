c63b3acecd4919f7b65a8e2bbfacfd32ec5cd3fc
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-10 21:35:50 +0000

    Create upload MVC

--- app/models/upload.js
+++ app/models/upload.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  attachment: DS.attr('file')
+  attachment: DS.attr('file'),
+  filename: DS.attr('string'),
+  createdAt: DS.attr('date')
 });
--- tests/unit/adapters/upload-test.js
+++ tests/unit/adapters/upload-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('adapter:post', 'PostAdapter', {
+moduleFor('adapter:upload', 'UploadAdapter', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
 });
