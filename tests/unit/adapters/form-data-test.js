da53c4a3d076f1dc2f63a9c6434458bcadf79c74
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-12 19:56:55 +0000

    Added unit test for form-data adapter

--- tests/unit/adapters/form-data-test.js
+++ tests/unit/adapters/form-data-test.js
@@ -13,3 +13,34 @@ test('it exists', function() {
   var adapter = this.subject();
   ok(adapter);
 });
+
+test('it correctly formats formData', function() {
+  var adapter = this.subject();
+
+  window.FormData = function() {
+    this.data = [];
+    this.append = function(key, value) {
+      var object = {};
+      object[key] = value;
+      this.data.push(object);
+    };
+  };
+
+  var testFormData = new window.FormData();
+
+  testFormData.append('taxonomy[id]', 1);
+  testFormData.append('taxonomy[name]', 'Google');
+
+  var options = {
+    data: {
+      taxonomy: {
+        id: 1,
+        name: 'Google'
+      }
+    }
+  };
+
+  var hash = adapter.ajaxOptions('/', 'POST', options);
+
+  deepEqual(hash.data, testFormData);
+});
