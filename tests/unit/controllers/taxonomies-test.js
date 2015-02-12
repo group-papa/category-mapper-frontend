ef5d0f21e9f80e12e5822646f8da358bc5833aa2
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-12 19:57:09 +0000

    Renamed taxonomies test

--- tests/unit/controllers/taxonomies-test.js
+++ tests/unit/controllers/taxonomies-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:taxonomy', 'TaxonomyController', {
+moduleFor('controller:taxonomies', 'TaxonomiesController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
