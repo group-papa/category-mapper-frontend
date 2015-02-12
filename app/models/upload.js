23770c70049e1af48436f38e5a858d81b8aa1050
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-12 09:48:12 +0000

    Make model attribute consistent

--- app/models/upload.js
+++ app/models/upload.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   attachment: DS.attr('file'),
   filename: DS.attr('string'),
-  createdAt: DS.attr('date')
+  dateCreated: DS.attr('date')
 });
