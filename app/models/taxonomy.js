38b2c20a29e9f82b60290dea21c70161274268dc
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-16 13:01:16 +0000

    Link taxonomy with category

--- app/models/taxonomy.js
+++ app/models/taxonomy.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   attachment: DS.attr('file'),
-  dateCreated: DS.attr('date')
+  dateCreated: DS.attr('date'),
+  categories: DS.hasMany('category')
 });
