77e597f24bc97cde09fe34808d16e7d6cc935fd6
Priyesh Patel <priyesh@priyesh.io>
2015-02-17 13:34:18 +0000

    Garbage collect deleted categories - fixes #19

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  dependentRelationship: null,
   click: function() {
     var item = this.get('item');
     var current = item.get('isSelected');
@@ -12,7 +13,20 @@ export default Ember.Component.extend({
   },
   actions: {
     delete: function() {
-      this.get('item').destroyRecord();
+      var item = this.get('item');
+      var dependentRelationship = this.get('dependentRelationship');
+
+      if (dependentRelationship !== null
+          && item._relationships[dependentRelationship]
+                                .manyArray.get('isLoaded')) {
+        item.get(dependentRelationship)
+          .toArray()
+          .forEach(function(entity) {
+              entity.unloadRecord();
+          });
+      }
+
+      item.destroyRecord();
     }
   }
 });
