c65b10a91a7b63226d2215933b963947a91e8098
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-02-21 17:39:58 +0000

    Working job page

--- app/routes/run-job.js
+++ app/routes/run-job.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-      return Ember.RSVP.hash({
-          taxonomies: this.store.find('taxonomy'),
-          uploads: this.store.find('upload')
-      });
+    //return this.store.find('product');
+    return Ember.RSVP.hash({
+      taxonomies: this.store.find('taxonomy'),
+      uploads: this.store.find('upload')
+    });
   }
 });
